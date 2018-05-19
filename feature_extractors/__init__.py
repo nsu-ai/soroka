a2f8b18899b46939403b090f35c0c17cb321253e
Ivan Bondarenko <bond005@yandex.ru>
2018-05-20 04:00:46 +0700

    Специальный FeatureUnion для фича-экстракторов, готовящих фичи для WordCNN.

--- feature_extractors/__init__.py
+++ feature_extractors/__init__.py
@@ -1,2 +1,3 @@
 from feature_extractors.embedding_extractor import EmbeddingExtractor
 from feature_extractors.shape_extractor import ShapeExtractor
+from feature_extractors.seq_feature_union import SeqFeatureUnion
