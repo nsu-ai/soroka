1972776299df91566cbf66d837ba2c3d9812a2ae
Ivan Bondarenko <bond005@yandex.ru>
2018-05-19 22:24:42 +0700

    Проведён рефакторинг класса EmbeddingExtractor - фича-экстрактора на основе word2vec.

--- feature_extractors/embedding_extractor.py
+++ feature_extractors/embedding_extractor.py
@@ -1,60 +1,92 @@
+from argparse import ArgumentParser
+import copy
 import os
 import gensim
 import numpy as np
 
-from sklearn.base import BaseEstimator, ClassifierMixin
-from sklearn.exceptions import NotFittedError
+from nltk import word_tokenize
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils.validation import check_is_fitted
 
-class EmbeddingExtractor(BaseEstimator, ClassifierMixin):
-    def __init__(self, word2vec_name = os.path.join('data', 'word2vec'), tokenizer = str.split):
-        self.tokenize = tokenizer
-        self.fitted = False
-        self.word2vec = gensim.models.KeyedVectors.load_word2vec_format(word2vec_name, binary = True, unicode_errors = 'ignore')
-        self.size = len(self.word2vec[list(self.word2vec.vocab.keys())[0]])
-        
-    def fit(self, X, y = None):
-        lengths = [len(self.tokenize(sentence)) for sentence in X]
-        self.max_sentence_length = int(np.mean(lengths) + np.std(lengths))
-        self.fitted = True
+
+class EmbeddingExtractor(BaseEstimator, TransformerMixin):
+    def __init__(self, word2vec_name=os.path.join(os.path.dirname(__file__), '..', 'data', 'word2vec_main.w2v'),
+                 tokenizer=word_tokenize, lowercase=False):
+        self.tokenizer = tokenizer
+        self.word2vec_name = word2vec_name
+        self.lowercase = lowercase
+
+    def fit(self, X, y=None):
+        self.check_X(X)
+        self.word2vec_ = gensim.models.KeyedVectors.load_word2vec_format(self.word2vec_name, binary=True,
+                                                                         unicode_errors='ignore')
+        self.size_ = len(self.word2vec_[list(self.word2vec_.vocab.keys())[0]])
+        lengths = [len(self.tokenizer(sentence)) for sentence in X]
+        self.max_sentence_length_ = int(np.mean(lengths) + np.std(lengths))
         return self
-    
-    def transform(self, X, y = None):
-        if not isinstance(X, list):
-            raise TypeError('X must be a list, but it is a {type}'.format(type = type(X)))
-        
-        if not self.fitted:
-            raise NotFittedError
-        
-        result = np.zeros((len(X), 1, self.max_sentence_length, self.size + 1))
+
+    def transform(self, X):
+        self.check_X(X)
+        check_is_fitted(self, ['max_sentence_length_'])
+        result = np.zeros((len(X), 1, self.max_sentence_length_, self.size_ + 1), dtype=np.float32)
         for line_number, line in enumerate(X):
-            for word_number, word in enumerate(self.tokenize(line)):
-                if word_number < self.max_sentence_length:
-                    try:
-                        result[line_number, 0, word_number] = np.hstack([self.word2vec[word], [0]])
-                    except KeyError:
-                        result[line_number, 0, word_number] = np.hstack([np.zeros(self.size), [1]])
-                else:
+            for word_number, word in enumerate(self.tokenizer(line.lower() if self.lowercase else line)):
+                if word_number >= self.max_sentence_length_:
                     break
-        
+                if word in self.word2vec_:
+                    result[line_number, 0, word_number, 0:self.size_] = self.word2vec_[word]
+                result[line_number, 0, word_number, self.size_] = 1.0
         return result
-    
-    def fit_transform(self, X, y = None):
-        return self.fit(X).transform(X)
-    
+
+    def check_X(self, X):
+        if (not isinstance(X, list)) and (not isinstance(X, tuple)) and (not isinstance(X, np.ndarray)):
+            raise TypeError('X must be a list, but it is a {type}'.format(type=type(X)))
+        if isinstance(X, np.ndarray):
+            if len(X.shape) != 1:
+                raise TypeError('X must be a 1-D array, but it is a {shape}-D array'.format(shape=len(X.shape)))
+
     def __setstate__(self, state):
-        self.tokenize = state['tokenize']
-        self.fitted = state['fitted']
-        self.word2vec = state['word2vec']
-        self.size = state['size']
-        if self.fitted:
-            self.max_sentence_length = state['max_sentence_length']
+        self.tokenizer = copy.deepcopy(state['tokenizer'])
+        self.word2vec_name = state['word2vec_name']
+        self.lowercase = state['lowercase']
+        if ('max_sentence_length_' in state) and ('size_' in state):
+            if hasattr(self, 'word2vec_'):
+                del self.word2vec_
+            self.max_sentence_length_ = state['max_sentence_length_']
+            self.size_ = state['size_']
+            self.word2vec_ = gensim.models.KeyedVectors.load_word2vec_format(self.word2vec_name, binary=True,
+                                                                             unicode_errors='ignore')
         return self
     
     def __getstate__(self):
-        result =  {'tokenize': self.tokenize, 'fitted': self.fitted,
-                'word2vec': self.word2vec, 'size': self.size}
-        
-        if self.fitted:
-            result['max_sentence_length'] = self.max_sentence_length
-        
+        result =  {'tokenizer': copy.deepcopy(self.tokenizer), 'word2vec_name': self.word2vec_name,
+                   'lowercase': self.lowercase}
+        if hasattr(self, 'max_sentence_length_'):
+            result['max_sentence_length_'] = self.max_sentence_length_
+        if hasattr(self, 'size_'):
+            result['size_'] = self.size_
         return result
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('-d', '--download', dest='downloaded_w2v_model', type=str, required=True,
+                        choices=['small', 'main', 'large'],
+                        help='Kind of a word2vec model for downloading (`small` model, `main` model or `large` model).')
+    args = parser.parse_args()
+
+    if args.downloaded_w2v_model == 'main':
+        model_url = 'http://panchenko.me/data/dsl-backup/w2v-ru/tenth.norm-sz500-w7-cb0-it5-min5.w2v'
+        model_name = os.path.join(os.path.dirname(__file__), '..', 'data', 'word2vec_main.w2v')
+    elif args.downloaded_w2v_model == 'large':
+        model_name = os.path.join(os.path.dirname(__file__), '..', 'data', 'word2vec_large.w2v')
+        model_url = 'http://panchenko.me/data/dsl-backup/w2v-ru/all.norm-sz500-w10-cb0-it3-min5.w2v'
+    else:
+        model_name = os.path.join(os.path.dirname(__file__), '..', 'data', 'word2vec_small.w2v')
+        model_url = 'http://panchenko.me/data/dsl-backup/w2v-ru/all.norm-sz100-w10-cb0-it1-min100.w2v'
+
+    import urllib.request
+    import shutil
+
+    with urllib.request.urlopen(model_url) as response, open(model_name, 'wb') as model_fp:
+        shutil.copyfileobj(response, model_fp)
--- feature_extractors/tests/test_embedding_extractor.py
+++ feature_extractors/tests/test_embedding_extractor.py
@@ -1,64 +1,100 @@
-import unittest
 import os
-import gensim
 import pickle
+import unittest
+
+import gensim
 import numpy as np
+import shutil
+import urllib.request
+
 from feature_extractors import EmbeddingExtractor
 from sklearn.exceptions import NotFittedError
 
+
 class TestEmbeddingExtractor(unittest.TestCase):
     def setUp(self):
-        self.path = os.path.join('data', 'word2vec.w2v')
+        self.model_path = os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'word2vec_small.w2v')
+        if not os.path.isfile(self.model_path):
+            model_url = 'http://panchenko.me/data/dsl-backup/w2v-ru/all.norm-sz100-w10-cb0-it1-min100.w2v'
+            with urllib.request.urlopen(model_url) as response, open(self.model_path, 'wb') as model_fp:
+                shutil.copyfileobj(response, model_fp)
+        self.tmp_extractor_name = os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'tmp_extractor.pkl')
+        self.fe = EmbeddingExtractor(word2vec_name=self.model_path, lowercase=True)
     
     def tearDown(self):
-        pass
+        if os.path.isfile(self.tmp_extractor_name):
+            os.remove(self.tmp_extractor_name)
+        if hasattr(self, 'fe'):
+            del self.fe
     
     def test_fit_positive01(self):
-        emb = EmbeddingExtractor(self.path)
-        self.assertIsInstance(emb.word2vec, gensim.models.KeyedVectors)
-        del emb
+        X_train = ['Ваш банк полный ацтой!', 'Ваш магаз — нормас', 'Мне пофиг на ваш ресторан']
+        self.assertTrue(hasattr(self.fe, 'tokenizer'))
+        self.assertTrue(hasattr(self.fe, 'word2vec_name'))
+        self.assertTrue(hasattr(self.fe, 'lowercase'))
+        self.assertFalse(hasattr(self.fe, 'word2vec_'))
+        self.assertFalse(hasattr(self.fe, 'size_'))
+        self.assertFalse(hasattr(self.fe, 'max_sentence_length_'))
+        self.assertEqual(self.fe.word2vec_name, self.model_path)
+        self.assertTrue(self.fe.lowercase)
+        self.fe.fit(X_train)
+        self.assertTrue(hasattr(self.fe, 'word2vec_'))
+        self.assertTrue(hasattr(self.fe, 'size_'))
+        self.assertTrue(hasattr(self.fe, 'max_sentence_length_'))
+        self.assertIsInstance(self.fe.word2vec_, gensim.models.KeyedVectors)
+        self.assertIsInstance(self.fe.size_, int)
+        self.assertIsInstance(self.fe.max_sentence_length_, int)
+        self.assertEqual(self.fe.size_, 100)
+        self.assertGreater(self.fe.max_sentence_length_, 0)
     
     def test_fit_negative01(self):
+        X_train = ['Ваш банк полный ацтой!', 'Ваш магаз — нормас', 'Мне пофиг на ваш ресторан']
+        emb = EmbeddingExtractor(word2vec_name='this_path_is_not_expected_to_exist', lowercase=True)
         with self.assertRaises(FileNotFoundError):
-            emb = EmbeddingExtractor('this_path_is_not_expected_to_exist')
+            emb.fit(X_train)
     
     def test_transform_positive01(self):
-        X = ['Ваш банк полный ацтой!', 'Ваш магаз — нормас', 'Мне пофиг на ваш ресторан']
-        emb = EmbeddingExtractor(self.path).fit(X)
-        X_transformed = emb.transform(X)
+        EPS = 1e-5
+        X = np.array(['Ваш банк полный ацтой!', 'Ваш магаз — нормас', 'Мне пофиг на ваш ресторан'])
+        X_transformed = self.fe.fit_transform(X)
+        self.assertIsInstance(X_transformed, np.ndarray)
         self.assertEqual(len(X_transformed.shape), 4)
         self.assertEqual(X_transformed.shape[0], len(X))
         self.assertEqual(X_transformed.shape[1], 1)
-        self.assertEqual(X_transformed.shape[2], emb.max_sentence_length)
-        self.assertEqual(X_transformed.shape[3], emb.size + 1)
-        del emb
+        self.assertEqual(X_transformed.shape[2], self.fe.max_sentence_length_)
+        self.assertEqual(X_transformed.shape[3], self.fe.size_ + 1)
+        for sentence_ind in range(len(X)):
+            n = min(self.fe.max_sentence_length_, len(self.fe.tokenizer(X[sentence_ind])))
+            all_zeros = True
+            for token_ind in range(n):
+                self.assertAlmostEqual(X_transformed[sentence_ind, 0, token_ind, self.fe.size_], 1.0, delta=EPS,
+                                       msg='Sentence {0}, token {1} from {2}'.format(sentence_ind, token_ind, n))
+                if np.sum(np.square(X_transformed[sentence_ind, 0, token_ind, 0:self.fe.size_])) > EPS:
+                    all_zeros = False
+            self.assertFalse(all_zeros, msg='Sentence {0}'.format(sentence_ind,))
+            for token_ind in range(n, self.fe.max_sentence_length_):
+                self.assertAlmostEqual(X_transformed[sentence_ind, 0, token_ind].tolist(), [0.0] * (self.fe.size_+ 1),
+                                       delta=EPS, msg='Sentence {0}, token {1}'.format(sentence_ind, token_ind))
     
     def test_transform_negative01(self):
         X = ['Ваш банк полный ацтой!', 'Ваш магаз — нормас', 'Мне пофиг на ваш ресторан']
-        with self.assertRaises(TypeError):
-            emb = EmbeddingExtractor(self.path).fit()
-            emb.transform(1)
-            del emb
-        
         with self.assertRaises(NotFittedError):
-            EmbeddingExtractor(self.path).transform(X)
+            self.fe.transform(X)
+        self.fe.fit(X)
+        with self.assertRaises(TypeError):
+            _ = self.fe.transform(1)
             
     def test_pickling_unpickling_positive01(self):
         X = ['Ваш банк полный ацтой!', 'Ваш магаз — нормас', 'Мне пофиг на ваш ресторан']
-        emb1 = EmbeddingExtractor(self.path).fit(X)
-        output1 = emb1.transform(X)
-        with open('emb.pkl', 'wb') as f:
-            pickle.dump(emb1, f)
-        
-        del emb1
-        
-        with open('emb.pkl', 'rb') as f:
-            emb2 = pickle.load(f)
-        
-        os.remove('emb.pkl')
-        output2 = emb2.transform(X)
+        output1 = self.fe.fit_transform(X)
+        with open(self.tmp_extractor_name, 'wb') as f:
+            pickle.dump(self.fe, f)
+        with open(self.tmp_extractor_name, 'rb') as f:
+            fe2 = pickle.load(f)
+        output2 = fe2.transform(X)
+        del fe2
         self.assertTrue(np.array_equal(output1, output2))
     
     
 if __name__ == '__main__':
-    unittest.main(verbosity = 2)
+    unittest.main(verbosity=2)
