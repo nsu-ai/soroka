bd97d3e37dcb831fdf72d079d3155b67b1269485
azabaykin <alexey.zabaykin@gmail.com>
2018-05-20 12:10:42 +0700

    recursive bypass works

--- crawler/crawler_01.py
+++ crawler/crawler_01.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
 from typing import List
+from urllib.parse import urlsplit
 
 # from googlesearch import search
 from requests import get
@@ -16,15 +17,47 @@ class Crawler01(Crawler):
         out = OrderedDict()
 
         for url in urls:
-            html_soup = self.get_bs4_from_url(url)
+            # detecting base url
+            base_url = "{0.scheme}://{0.netloc}/".format(urlsplit(url))
 
-            paragraphs = []
-            for p in html_soup.find_all('p'):
-                txt = p.getText().strip()
-                if txt:
-                    paragraphs.append(txt)
+            # depth == 1
+            html_soup = self.get_bs4_from_url(url)
+            paragraphs = self.get_paragraphs(html_soup)
             out[url] = paragraphs
+            urls_depth_2 = self.get_links_on_page(html_soup, base_url=base_url, old_links=out.keys())
+
+            if depth > 1:
+                for url_2 in urls_depth_2:
+                    html_soup = self.get_bs4_from_url(url_2)
+                    paragraphs = self.get_paragraphs(html_soup)
+                    out[url_2] = paragraphs
+                    urls_depth_3 = self.get_links_on_page(html_soup, base_url=base_url, old_links=out.keys())
+            if depth > 2:
+                for url_3 in urls_depth_3:
+                    html_soup = self.get_bs4_from_url(url_3)
+                    paragraphs = self.get_paragraphs(html_soup)
+                    out[url_3] = paragraphs
+                    urls_depth_4 = self.get_links_on_page(html_soup, base_url=base_url, old_links=out.keys())
+            if depth > 3:
+                for url_4 in urls_depth_4:
+                    html_soup = self.get_bs4_from_url(url_4)
+                    paragraphs = self.get_paragraphs(html_soup)
+                    out[url_4] = paragraphs
+                    urls_depth_5 = self.get_links_on_page(html_soup, base_url=base_url, old_links=out.keys())
+        # for k, el in out.items():
+        #     print(k, '!!!')
+        #     for e in el:
+        #         print(e)
         return out
+
+    @staticmethod
+    def get_paragraphs(html_soup):
+        paragraphs = []
+        for p in html_soup.find_all('p'):
+            txt = p.getText().strip()
+            if txt:
+                paragraphs.append(txt)
+        return paragraphs
 
     @staticmethod
     def get_bs4_from_url(url):
@@ -39,13 +72,15 @@ class Crawler01(Crawler):
         html_soup = BeautifulSoup(html_text, 'html.parser')
         return html_soup
 
-    def get_links_on_page(self, html_soup, base_url):
+    @staticmethod
+    def get_links_on_page(html_soup, base_url, old_links=[]):
         links = [a.get('href') for a in html_soup.find_all('a', href=True)]
 
         def add_http(url):
-            return url if url.startswith('http') else base_url + '/' + url
+            return url if url.startswith(base_url) else base_url + url
 
         links = [add_http(l) for l in links]
+        links = [l for l in links if l not in old_links]
         return links
 
     def get_all_pages(self, url):
--- crawler/tests/crawler_test.py
+++ crawler/tests/crawler_test.py
@@ -9,9 +9,18 @@ class TestExtractor(unittest.TestCase):
     def test_init_positive01(self):
         my_crawler = Crawler01()
         site_to_check = 'http://zabaykin.ru'
-        res = my_crawler.load_and_tokenize([site_to_check])
+        res = my_crawler.load_and_tokenize([site_to_check], depth=1)
         # print(res)
+        # check number of paragraphs
         self.assertEqual(len(res[site_to_check]), 63)
+
+    def test_init_positive02(self):
+        my_crawler = Crawler01()
+        site_to_check = 'http://zabaykin.ru/?p=505'
+        res = my_crawler.load_and_tokenize([site_to_check], depth=2)
+        # print(res)
+        # check number of parsed urls
+        self.assertEqual(len(list(set(res.keys()))), 36)
 
 
 if __name__ == '__main__':
