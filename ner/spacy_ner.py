a564c7c3799ea4f1e511370db5f6bdc654db6cb2
Anastasia Malysheva <nastiamalysh@mail.ru>
2018-05-20 16:00:48 +0700

    Распознаватель именованных сущностей spacy. Для работы необходимо скачать модель (python3 -m spacy download xx_ent_wiki_sm).

---
+++ ner/spacy_ner.py
@@ -0,0 +1,70 @@
+from collections import OrderedDict
+import spacy
+import pymorphy2
+
+class SpacyNamedEntityRecognizer(object):
+    """ Класс для распознавания именованных сущностей и фильтрации веб-контента только по нужным нам сущностям.
+
+    """
+    def __init__(self):
+        pass
+
+    def filter_content(self, who: str, is_person: bool, web_content: OrderedDict) -> OrderedDict:
+        """ Отфильтровать входной веб-контент, удалив из него абзацы, где нет упоминаний о нужных нам людях или фирмах.
+
+        Входной веб-контент представляет собой словарь, ключами которого являются строковые описания ранее пропарсенных
+        URL-ов, а значениями - списки строк, т.е. текстовый контент каждого URL-а, разбитый на последовательность
+        абзацев. Пример:
+
+        {
+            "www.abc.com": [
+                "мама мыла раму",
+                "корова молоко даёт"
+            ],
+            "https://hello.org": [
+                "Здравствуй, мир!",
+                "И тебе исполать, добрый молодец!",
+                "Доброго здоровьица, девица краса.",
+                "Здесь что, все здороваются?"
+            ]
+        }
+
+        Данный метод вернёт такую же структуру данных, но те абзацы, в которых не будет упоминаний о нужном нам человеке
+        или организации, будут удалены. Если получится так, что во всех абзацах какого-то URL-а нет ни одного упоминания
+        искомой именованной сущности, то удаляется весь URL.
+
+        :param who: строка с описанием искомой сущности (напр., "Вася Пупкин" или "Рога и копыта")
+        :param is_person: искомая сущность - это человек (True) или организация (False)?
+        :param web_content: словарь текстового контента, разбитого на абзацы, для всех обойдённых URL-ов
+
+        :return тот же самый словарь, что и web_content, только отфильтрованный.
+
+        """
+        nlp = spacy.load('xx_ent_wiki_sm')
+        morph = pymorphy2.MorphAnalyzer()
+        for url in list(web_content.keys()):
+            url_fl = 0
+            for i in range(len(web_content[url])):
+                #print(document)
+                doc_fl = 0
+                doc = nlp(web_content[url][i])
+                for ent in doc.ents:
+                    #print(ent, ent.label_, type(ent.label_))
+                    if is_person:
+                        #print('Is person')
+                        if ent.label_ == 'PER':
+                            #print(ent.text, who)
+                            if morph.parse(ent.text.lower())[0].normal_form == morph.parse(who.lower())[0].normal_form:
+                                doc_fl += 1
+                                url_fl += 1
+                    else:
+                        #print('Not person')
+                        if ent.label_ == 'ORG' or ent.label_== 'LOC':
+                            if morph.parse(ent.text.lower())[0].normal_form == morph.parse(who.lower())[0].normal_form:
+                                doc_fl += 1
+                                url_fl += 1
+                if doc_fl == 0:
+                    del web_content[url][i]
+            if url_fl == 0:
+                del web_content[url]
+        return web_content
---
+++ ner/test/test_spacy_ner.py
@@ -0,0 +1,33 @@
+import unittest
+import copy
+from ner.spacy_ner import SpacyNamedEntityRecognizer
+
+
+class TestSpacyNer(unittest.TestCase):
+
+
+    def test_delete_by_person(self):
+
+        test_content = {'url1': ['Наташа помогла Андрею с домашней работой. Наташа хороший человек.', 'Она большая молодец'], 'url2': ['Сегодня день рождения Алексеевой, поздравим ее']}
+        nr = SpacyNamedEntityRecognizer()
+        content_to_transform = copy.deepcopy(test_content)
+        res = nr.filter_content('Наташа', True, content_to_transform)
+        print('RES', res)
+        del test_content['url1'][1]
+        del test_content['url2']
+        print('Content', test_content)
+        self.assertEqual(res, test_content)
+
+    def test_delete_by_org(self):
+        test_content = {'url1': ['В последнее время не очень люблю ходить в Пятерочку, они отвратительно относятся к своим сотрудникам. В магазине Пятерочка продаются самые разные вещи'], 'url2': ['Вася продал акции'], 'url3': ['Магазин Пятерочка набирает сотрудников на работу']}
+        nr = SpacyNamedEntityRecognizer()
+        content_to_transform = copy.deepcopy(test_content)
+        res = nr.filter_content('Пятерочка', False, content_to_transform)
+        print('Res', res)
+        del test_content['url2']
+        print('Content', test_content)
+        self.assertEqual(res, test_content)
+
+
+if __name__ == '__main__':
+    unittest.main()
