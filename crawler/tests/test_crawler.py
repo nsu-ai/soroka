860dfdfd4271a2b884c8548147f7c9f2f93f284a
azabaykin <azabaykin@openprovider.ru>
2018-05-20 15:35:04 +0700

    added test for type checking

--- crawler/tests/test_crawler.py
+++ crawler/tests/test_crawler.py
@@ -1,5 +1,7 @@
 from crawler.crawler import Crawler
 import unittest
+from collections import OrderedDict
+from typing import List
 
 
 class TestExtractor(unittest.TestCase):
@@ -21,6 +23,16 @@ class TestExtractor(unittest.TestCase):
         # print(res)
         # check number of parsed urls
         self.assertEqual(len(list(set(res.keys()))), 36)
+
+    def test_structure_positive_01(self):
+        my_crawler = Crawler()
+        site_to_check = 'http://zabaykin.ru/?p=505'
+        res = my_crawler.load_and_tokenize([site_to_check], depth=2)
+        self.assertIsInstance(res, OrderedDict)
+        for k, el in res.items():
+            self.assertIsInstance(k, str)
+            self.assertTrue(k.startswith('http'))
+            self.assertIsInstance(el, List[str])
 
 
 if __name__ == '__main__':
